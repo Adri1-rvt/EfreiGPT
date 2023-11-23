4c94e7d82feecc503f6c297a26d905148a781674
Adri1 <94009526+Tevir22@users.noreply.github.com>
2023-11-23 12:04:26 +0100

    Fourth commit

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 Auteurs : Gabriel PRIEUR, Adrien RIVET
 """
 
+
 """----------IMPORTATTION DES MODULES ET FONCTIONS EXTERNES----------"""
-import tfidf_matrice_calculation
+import time
 
 """----------DECLARATION DES FONCTIONS UTILISATEUR----------"""
 
@@ -17,49 +18,50 @@ print("""
  | |__  | |_ _ __ ___ _  | |  __| |__) | | |   
  |  __| |  _| '__/ _ \ | | | |_ |  ___/  | |   
  | |____| | | | |  __/ | | |__| | |      | |   
- |______|_| |_|  \___|_|  \_____|_|      |_|   
+ |______|_| |_|  \___|_|  \_____|_|      |_| v1.1
 """)
 print("-----Bonjour et bienvenue sur EfreiGPT !-----")
 
-fonctionality1 = "Afficher la liste des mots les moins importants dans le corpus de documents"
-fonctionality2 = "Afficher le(s) mot(s) ayant le score TD-IDF le plus élevé"
-fonctionality3 = "Indiquer le(s) mot(s) le(s) plus répété(s) par le président Chirac"
-fonctionality4 = "Indiquer le(s) nom(s) du (des) président(s) qui a (ont) parlé de la « Nation » et celui qui l’a répété le plus de fois"
-fonctionality5 = "Indiquer le premier président à parler du climat et/ou de l’écologie"
-fonctionality6 = "Hormis les mots dits « non importants », quel(s) est(sont) le(s) mot(s) que tous les présidents ont évoqués"
+time.sleep(1)
 
+message = "Que désirez-vous faire ?"
+for letter in message:
+    print(letter, end="")
+    time.sleep(0.1)
+print()
+time.sleep(1)
+fonctionality_list = ["Afficher la liste des mots les moins importants dans le corpus de documents",
+                      "Afficher le mot ayant le score TF-IDF le plus élevé",
+                      "Indiquer le(s) mot(s) le(s) plus répété(s) par le président Chirac",
+                      "Indiquer les noms des présidents qui ont parlé de la « Nation » et celui qui l’a répété le plus de fois",
+                      "Indiquer le premier président à parler du climat et/ou de l’écologie",
+                      "Hormis les mots dits « non importants », quels sont les mots que tous les présidents ont évoqués"]
+cpt = 1
+for fonctionnality in fonctionality_list:
+    print(f"{cpt}- {fonctionnality}")
+    cpt += 1
+    time.sleep(0.5)
 
-print("Que désirez-vous faire ?\n",
-      f"1- {fonctionality1}\n",
-      f"2- {fonctionality2}\n",
-      f"3- {fonctionality3}\n",
-      f"4- {fonctionality4}\n",
-      f"5- {fonctionality5}\n",
-      f"6- {fonctionality6}")
 print("---------------------------------------------")
 number = int(input("Tapez le numéro choisi : "))
 while number <= 0 or number > 6:
     number = int(input("Tapez le numéro choisi : "))
 
 if number == 1:
-    print(f"Vous avez choisi : {fonctionality1}\n"+
+    print(f"Vous avez choisi : {fonctionality_list[1]}\n"+
           "Réponse :")
-
-    # Obtenir la matrice TF-IDF
-    tfidf_matrix = tfidf_matrice_calculation.tfidf_matrix("cleaned")
-
 elif number == 2:
-    print(f"Vous avez choisi : {fonctionality2}\n"+
+    print(f"Vous avez choisi : {fonctionality_list[2]}\n"+
           "Réponse :")
 elif number == 3:
-    print(f"Vous avez choisi : {fonctionality3}\n"+
+    print(f"Vous avez choisi : {fonctionality_list[3]}\n"+
           "Réponse :")
 elif number == 4:
-    print(f"Vous avez choisi : {fonctionality4}\n"+
+    print(f"Vous avez choisi : {fonctionality_list[4]}\n"+
           "Réponse :")
 elif number == 5:
-    print(f"Vous avez choisi : {fonctionality5}\n"+
+    print(f"Vous avez choisi : {fonctionality_list[5]}\n"+
           "Réponse :")
 elif number == 6:
-    print(f"Vous avez choisi : {fonctionality6}\n"+
+    print(f"Vous avez choisi : {fonctionality_list[6]}\n"+
           "Réponse :")
--- tfidf_matrice_calculation.py
+++ tfidf_matrice_calculation.py
@@ -5,7 +5,7 @@ Auteurs : Gabriel PRIEUR, Adrien RIVET
 """----------IMPORTATTION DES MODULES ET FONCTIONS EXTERNES----------"""
 import math
 import os
-
+from text_treatment import extract_president_name
 
 """----------DECLARATION DES FONCTIONS UTILISATEUR----------"""
 
@@ -29,9 +29,9 @@ def tf_calculation(f):
 def idf_calculation(corpus_dir):
     word_doc_count = {}
     total_number_of_files = len(os.listdir(corpus_dir))
-    for filename in os.listdir(corpus_dir):
-        with open(f"{corpus_dir}\\{filename}", 'r') as file:
-            document = file.read()
+    for file_name in os.listdir(corpus_dir):
+        with open(f"{corpus_dir}\\{file_name}", 'r') as f:
+            document = f.read()
             words_list = document.split()
             unique_words = set(words_list)
 
@@ -60,8 +60,8 @@ def tfidf_matrix(corpus_dir):
     idf_scores = idf_calculation(corpus_dir)
 
     for filename in os.listdir(corpus_dir):
-        with open(f"{corpus_dir}\\{filename}", 'r') as file2:
-            tf_scores = tf_calculation(file2)
+        with open(f"{corpus_dir}\\{filename}", 'r') as f:
+            tf_scores = tf_calculation(f)
             tfidf_scores = {}
             for word in tf_scores:
                 tf = tf_scores[word]
@@ -77,3 +77,136 @@ def tfidf_matrix(corpus_dir):
 # Obtenir la matrice TF-IDF
 tfidf_matrix_result = tfidf_matrix("cleaned")
 print(tfidf_matrix_result)
+
+
+
+
+
+"""FONCTIONNALITES A DEVELOPPER"""
+
+#1 Afficher la liste des mots les moins importants dans le corpus de documents
+print("\n----------------------------------------------------------------------")
+L = []
+for tfidf_scores in tfidf_matrix_result:
+    for word in tfidf_scores:
+        if tfidf_scores[word] == 0:
+            L.append(word)
+print("Les mots les moins importants sur le corpus de documents sont :", end=" ")
+
+for element in set(L):
+    print(f'"{element}",', end=" ")
+print()
+
+"""A modifier, le code estime qu'un mot est non-important si son score tfidf = 0 dans un document, or il faut que ce soit le cas dans chaque document"""
+
+
+
+#2 Afficher le mot ayant le score TF-IDF le plus élevé
+print("\n2----------------------------------------------------------------------")
+max_tfidf = 0
+max_word = ""
+for tfidf_scores in tfidf_matrix_result:
+    for word in tfidf_scores:
+        if tfidf_scores[word] > max_tfidf:
+            max_tfidf = tfidf_scores[word]
+            max_word = word
+
+print(f'Le mot ayant le score TF-IDF le plus élevé est le mot "{max_word}", qui a un score TF-IDF égal à {max_tfidf}.')
+
+
+
+#3 Indiquer le(s) mot(s) le(s) plus répété(s) par le président Chirac
+print("\n3----------------------------------------------------------------------")
+most_repeated_frequency = 0
+most_repeated_word = ""
+
+text_list = os.listdir("cleaned")
+for text in text_list:
+    if "Chirac" in text:
+        with open(f"cleaned\\{text}", "r") as f:
+            word_frequency = {}
+            elements = f.read()
+            words = elements.split()
+            for word in words:
+                if word in word_frequency:
+                    word_frequency[word] += 1
+                else:
+                    word_frequency[word] = 1
+
+            for word, frequency in word_frequency.items():
+                if frequency > most_repeated_frequency:
+                    most_repeated_frequency = frequency
+                    most_repeated_word = word
+
+print(f'''Le mot le plus répété par le président Chrirac est le mot "{most_repeated_word}", qu'il a prononcé {most_repeated_frequency} fois.''')
+
+
+
+#4 Indiquer les noms des présidents qui ont parlé de la « Nation » et celui qui l’a répété le plus de fois
+print("\n4----------------------------------------------------------------------")
+L = []
+maximum = 0
+text_list = os.listdir("cleaned")
+for text_name in text_list:
+    with open(f"cleaned\\{text_name}", "r") as f:
+        tf = tf_calculation(f)
+        if "nation" in tf.keys():
+            president_name = extract_president_name(text_name)
+            L.append(president_name)
+            if tf["nation"] >= maximum:
+                maximum = tf["nation"]
+                president = president_name
+cpt = 1
+print("Les présidents qui ont parlé de la « Nation » sont",end=" ")
+for president_name in set(L):
+    if cpt == len(set(L))-1:
+        print(f"{president_name} et", end=" ")
+    elif cpt != len(set(L)):
+        print(f"{president_name},", end=" ")
+    else:
+        print(f"{president_name}.")
+    cpt += 1
+print(f"C'est le président {president} qui l'a répété le plus de fois.")
+
+"""A modifier : pour les présidents qui ont plusieurs textes, il faut additionner les 2"""
+
+
+
+#5 Indiquer le premier président à parler du climat et/ou de l’écologie
+print("\n5----------------------------------------------------------------------")
+search_word = "climat"
+search_word2 = "écologie"
+text_list = os.listdir("cleaned")
+for text_name in text_list:
+    with open(f"cleaned\\{text_name}", "r") as f:
+        document = f.read()
+        word_list = document.split()
+        if (search_word in word_list) or (search_word2 in word_list):
+            ecological_president = extract_president_name(text_name)
+print(f"Le premier président à parler du climat et/ou de l’écologie est le président {ecological_president}.")
+
+"""A modifier : faire liste odre présidents dans le temps"""
+
+
+
+#6 Hormis les mots dits « non importants », quels sont les mots que tous les présidents ont évoqués
+print("\n6----------------------------------------------------------------------")
+common_words = {}
+text_list = os.listdir("cleaned")
+for text_name in text_list:
+    with open(f"cleaned\\{text_name}", "r") as f:
+        document = f.read()
+        word_list = document.split()
+        for word in set(word_list):
+            if word in common_words.keys():
+                common_words[word] += 1
+            else:
+                common_words[word] = 1
+
+print("Hormis les mots dits « non importants », les mots que tous les présidents ont évoqués sont :", end=" ")
+for word in common_words:
+    if common_words[word] == 8:
+        print(f'"{word}",', end=" ")
+
+"""A modifier : pour les présidents qui ont plusieurs textes, il faut additionner les 2"""
+"""A modifier : enlever les mots non-importants de la liste"""
